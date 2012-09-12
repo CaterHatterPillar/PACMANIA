12f4e99b00d3340c4c447c16654dd73c4e73fa02
CaterHatterPillar@gmail.com <CaterHatterPillar@gmail.com>
2012-09-12 16:22:42 +0200

    Used to group common resources

---
+++ Messaging/Common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "assert.h"
+#include <stdio.h>
+
+/*Lists*/
+#include <vector>
+#include <queue>
+#include <map>
+
+#include <string>
+
+#endif //COMMON_H
