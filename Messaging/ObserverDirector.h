0635d6e84710a7341c63a4011fcc49e23fd7d30b
CaterHatterPillar@gmail.com <CaterHatterPillar@gmail.com>
2012-09-12 16:27:50 +0200

    Central messaging unit. Components may subscribe and be forwarded messages sent in engine.

---
+++ Messaging/ObserverDirector.h
@@ -0,0 +1,28 @@
+#ifndef OBSERVERDIRECTOR_H
+#define OBSERVERDIRECTOR_H
+
+#include "Common.h"
+#include "Observer.h"
+
+/*Msgs*/
+#include "SubscriptionMsg.h"
+
+class ObserverDirector : public Component
+{
+private:
+	std::vector<Observer*>* observers;
+
+	void subscribeMsg(Msg* msg);
+	void subscribe(
+		Component*	subscriber, 
+		MsgType		subscription);
+protected:
+public:
+	ObserverDirector();
+	~ObserverDirector();
+
+	void init();
+	void update(double delta);
+};
+
+#endif //OBSERVERDIRECTOR_H
