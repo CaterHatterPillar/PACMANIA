050ea86d5f127b9bfd36ba9f412d457bfae85c4d
CaterHatterPillar@gmail.com <CaterHatterPillar@gmail.com>
2012-09-14 16:06:33 +0200

    Moved misplaced files

---
+++ SourceCode/Component.h
@@ -0,0 +1,25 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include "../Common.h"
+#include "Msg.h"
+
+class Component
+{
+private:
+	std::queue<Msg*> msgs;
+protected:
+
+	Msg* pop();
+	Msg* peek();
+public:
+	Component();
+	virtual ~Component();
+
+	virtual void update(double delta)	= 0;
+	virtual void init()					= 0;
+
+	void push(Msg* msg);
+};
+
+#endif //COMPONENT_H
