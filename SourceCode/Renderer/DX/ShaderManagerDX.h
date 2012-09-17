a11e833b52a370f8531c7f4cff8ecbfc520e67c3
L0mion <davidgrelsson@gmail.com>
2012-09-17 15:43:17 +0200

    Created a ShaderManager class

---
+++ SourceCode/Renderer/DX/ShaderManagerDX.h
@@ -0,0 +1,37 @@
+#ifndef SHADERMANAGERDX_H
+#define SHADERMANAGERDX_H
+
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <D3DX10.h>
+#include <D3Dcompiler.h>
+
+#include <Windows.h>
+#include <WindowsX.h>
+
+class ShaderManagerDX
+{
+private:
+	ID3D11Device* device;
+	ID3D11DeviceContext* devcon;
+
+	D3D_FEATURE_LEVEL featureLevel;
+
+	ID3D11Buffer* cBufferPerFrame;
+
+	ID3D11VertexShader* vertexShader;
+	ID3D11PixelShader* pixelShader;
+	ID3D11InputLayout* inputLayout;
+
+	ID3D10Blob* vs;
+	ID3D10Blob* ps;
+	ID3D10Blob* error;
+
+public:
+	ShaderManagerDX(ID3D11Device* device, ID3D11DeviceContext* devcon, D3D_FEATURE_LEVEL);
+	~ShaderManagerDX();
+
+	void initialize();
+};
+
+#endif
