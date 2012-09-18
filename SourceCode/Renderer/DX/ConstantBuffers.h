0ee99fc84cc42e0a98f089649bfb9efb319001b2
L0mion <davidgrelsson@gmail.com>
2012-09-18 09:04:19 +0200

    Forgot to add ConstantBuffers.h in last commit

---
+++ SourceCode/Renderer/DX/ConstantBuffers.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTBUFFERS_H
+#define CONSTANTBUFFERS_H
+
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <D3DX10.h>
+
+struct CBufferPerFrame
+{
+	D3DXMATRIX final;
+	D3DXMATRIX world;
+}
+
+#endif
