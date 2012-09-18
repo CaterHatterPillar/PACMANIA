1549714579b1e8eb9e590e868da6f3d94b8c8199
CaterHatterPillar@gmail.com <CaterHatterPillar@gmail.com>
2012-09-18 09:19:14 +0200

    Added rendermsg

---
+++ SourceCode/Messaging/MsgRender.h
@@ -0,0 +1,26 @@
+#ifndef MSGRENDER_H
+#define MSGRENDER_H
+
+#include "Msg.h"
+#include "../Renderer/GraphicsContainer.h"
+
+class MsgRender : public Msg
+{
+private:
+	GraphicsContainer* graphicsContainer;
+protected:
+public:
+	MsgRender(GraphicsContainer* graphicsContainer) : Msg(RENDER)
+	{
+		this->graphicsContainer = graphicsContainer;
+	}
+	MsgRender(MsgRender* msg) : Msg(RENDER)
+	{
+		this->graphicsContainer = msg->getGraphicsContainer();
+	}
+	~MsgRender() {}
+
+	GraphicsContainer* getGraphicsContainer() { return graphicsContainer; }
+};
+
+#endif //MSGRENDER_H
