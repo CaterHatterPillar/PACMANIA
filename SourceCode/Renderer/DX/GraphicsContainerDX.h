484b2feb7ab4d59cf32644545692e6d2c82ba946
L0mion <davidgrelsson@gmail.com>
2012-09-25 15:45:21 +0200

    Added enum to describe which shader to use in GraphicsContainer

---
+++ SourceCode/Renderer/DX/GraphicsContainerDX.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICSCONTAINERDX_H
+#define GRAPHICSCONTAINERDX_H
+
+#include "../GraphicsContainer.h"
+
+class GraphicsContainer : public GraphicsContainer
+{
+private:
+public:
+};
+
+#endif //GRAPHICSCONTAINERDX_H
--- SourceCode/Renderer/GraphicsContainer.h
+++ SourceCode/Renderer/GraphicsContainer.h
@@ -1,6 +1,12 @@
 #ifndef GRAPHICSCONTAINER_H
 #define GRAPHICSCONTAINER_H
 
+enum ShaderId
+{
+	VERTEX_SHADER_DEFAULT,
+	PIXEL_SHADER_DEFAULT
+};
+
 //Make abstract
 class GraphicsContainer
 {
@@ -8,6 +14,8 @@ private:
 protected:
 	unsigned int numVertices;
 	unsigned int numIndices;
+	Shader vertexShaderId;
+	Shader pixelShaderId;
 public:
 	GraphicsContainer(unsigned int numVertices, unsigned int numIndices)
 	{
@@ -15,6 +23,9 @@ public:
 		this->numIndices = numIndices;
 	}
 	virtual ~GraphicsContainer() {}
+	
+	Shader getVertexShaderId(){ return vertexShaderId;}
+	Shader getPixelShaderId(){ return pixelShaderId;}
 };
 
 #endif //GRAPHICSCONTAINER_H
