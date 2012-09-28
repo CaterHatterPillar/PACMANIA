9e0b367b5e98a0cbc354f29683faac9530324401
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-09-28 16:40:42 +0200

    -

--- SourceCode/Renderer/Vertices/PosNormTex.h
+++ SourceCode/Renderer/Vertices/PosNormTex.h
@@ -3,6 +3,14 @@
 
 #include "PosNorm.h"
 
+enum POS_NORM_TEX
+{
+	PosNormTex_POS,
+	PosNormTex_NORM,
+	PosNormTex_TEX,
+	PosNormTex_NUM_ATTRIBUTES
+};
+
 struct PosNormTex : public PosNorm
 {
 	VecF2 tex;
