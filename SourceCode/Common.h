af202368654f7ad2e63a539a34c9fdc6930f65ff
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-09-28 16:17:41 +0200

    Added string-cast function

--- SourceCode/Common.h
+++ SourceCode/Common.h
@@ -10,6 +10,7 @@
 #include <map>
 
 #include <string>
+#include <sstream>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -17,5 +18,13 @@
 #define F_O_V	60
 #define Z_NEAR	1
 #define Z_FAR	1000
+
+template <class T>
+inline std::string TO_STRING(const T& t)
+{
+	std::stringstream ss;
+	ss << t;
+	return ss.str();
+}
 
 #endif //COMMON_H
