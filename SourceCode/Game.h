cee63233989426865dc5026addaf81f12c265eee
CaterHatterPillar@gmail.com <CaterHatterPillar@gmail.com>
2012-10-04 10:24:33 +0200

    Now deleting modules passed to Game-class

--- SourceCode/Game.h
+++ SourceCode/Game.h
@@ -41,50 +41,15 @@ public:
 	}
 	~Game()
 	{
+		if(pacman)
+			delete pacman;
+		if(camera)
+			delete camera;
+		if(window)
+			delete window;
+		if(renderer)
+			delete renderer;
 	}
 };
 
 #endif //GAME_H
-
-/*
-gameEntity = new GameEntity(VecF3(1.0f, 1.0f, 1.0f), VecF3(1.0f, 1.0f, 1.0f), VecF3(1.0f, 1.0f, 1.0f));
-std::vector<PosNormTex>* vertices = new std::vector<PosNormTex>();
-PosNormTex vertex;
-vertex.pos = VecF3(-1.0f, -1.0f, 0.0f);
-vertices->push_back(vertex);
-vertex.pos = VecF3(0.0f, -1.0f, 1.0f);
-vertices->push_back(vertex);
-vertex.pos = VecF3(1.0f, -1.0f, 0.0f);
-vertices->push_back(vertex);
-vertex.pos = VecF3(0.0f, 1.0f, 0.0f);
-vertices->push_back(vertex);
-std::vector<unsigned int>* indices = new std::vector<unsigned int>();
-indices->push_back(0);
-indices->push_back(3);
-indices->push_back(1);
-indices->push_back(1);
-indices->push_back(3);
-indices->push_back(2);
-indices->push_back(2);
-indices->push_back(3);
-indices->push_back(0);
-indices->push_back(0);
-indices->push_back(2);
-indices->push_back(1);
-unsigned int numVertices = 4;
-unsigned int numIndices = 12;
-unsigned int numFaces = 4;
-unsigned int stride = sizeof(PosNormTex);
-unsigned int offset = 0;
-GraphicsContainerGL* graphicsContainer = new GraphicsContainerGL(
-VERTEX_SHADER_DEFAULT,
-PIXEL_SHADER_DEFAULT,
-vertices,
-indices,
-numVertices,
-numIndices,
-numFaces,
-stride,
-offset);
-gameEntity->setGraphicsContainer(graphicsContainer);
-*/
