1085b22aa6f645d8d3d5f016edb28fe5cb8fca1e
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-10-12 16:51:10 +0200

    Boom Boom Yadda Yadda Boom Bang

--- SourceCode/GL/GameEntityFactoryGL.cpp
+++ SourceCode/GL/GameEntityFactoryGL.cpp
@@ -42,11 +42,67 @@ GameEntity* GameEntityFactoryGL::createGhost(VecF3 position)
 }
 GameEntity* GameEntityFactoryGL::createPill(VecF3 position)
 {
-	return NULL;
+	GameEntity* entity = new GameEntity();
+	entity->setPosition(position);
+	
+	std::vector<PosNormTex>*	vertices	= createVerticesCube();
+	std::vector<unsigned int>*	indices		= createIndicesCube();
+
+	ShaderId vsID	= VERTEX_SHADER_DEFAULT;
+	ShaderId fsID	= PIXEL_SHADER_DEFAULT;
+	TextureId texID	= TEXTURE_PILL;
+	unsigned int numVertices	= vertices->size();
+	unsigned int numIndices		= indices->size();
+	unsigned int numFaces		= 12;
+	unsigned int stride			= sizeof(PosNormTex);
+	unsigned int offset			= 0;
+
+	GraphicsContainer* graphicsContainer = new GraphicsContainerGL( 
+		vsID, 
+		fsID, 
+		texID,
+		vertices, 
+		indices, 
+		numVertices, 
+		numIndices, 
+		numFaces, 
+		stride, 
+		offset);
+	entity->setGraphicsContainer(graphicsContainer);
+
+	return entity;
 }
 GameEntity* GameEntityFactoryGL::createBloodyPill(VecF3 position)
 {
-	return NULL;
+	GameEntity* entity = new GameEntity();
+	entity->setPosition(position);
+	
+	std::vector<PosNormTex>*	vertices	= createVerticesCube();
+	std::vector<unsigned int>*	indices		= createIndicesCube();
+
+	ShaderId vsID	= VERTEX_SHADER_DEFAULT;
+	ShaderId fsID	= PIXEL_SHADER_DEFAULT;
+	TextureId texID	= TEXTURE_PILL_BLOODY;
+	unsigned int numVertices	= vertices->size();
+	unsigned int numIndices		= indices->size();
+	unsigned int numFaces		= 12;
+	unsigned int stride			= sizeof(PosNormTex);
+	unsigned int offset			= 0;
+
+	GraphicsContainer* graphicsContainer = new GraphicsContainerGL( 
+		vsID, 
+		fsID, 
+		texID,
+		vertices, 
+		indices, 
+		numVertices, 
+		numIndices, 
+		numFaces, 
+		stride, 
+		offset);
+	entity->setGraphicsContainer(graphicsContainer);
+
+	return entity;
 }
 
 GameEntityFactoryGL::GameEntityFactoryGL() : GameEntityFactory()
