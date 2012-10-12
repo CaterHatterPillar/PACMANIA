db2f81c826b4f94717bf7d3962d0497eb9f59965
L0mion <davidgrelsson@gmail.com>
2012-10-12 15:43:01 +0200

    Tested pill creation in Game::run(), since this is not yet supported in GL the code has been commented out

--- SourceCode/Game/Game.cpp
+++ SourceCode/Game/Game.cpp
@@ -4,11 +4,16 @@ void Game::run()
 {
 	GameEntity* pacman = entityFac->createPacman(VecF3(0.0f, 0.0f, 0.0f));	//tmep
 
+//	GameEntity* pill = entityFac->createPill(VecF3(4.0f, 0.0f, 0.0f));
+//	GameEntity* bloodyPill = entityFac->createBloodyPill(VecF3(-3.0f, -2.0f, 3.0f)); 
+
 	do
 	{
 		double delta = tickTimer();
 
 		pacman->update(delta);
+//		pill->update(delta);
+//		bloodyPill->update(delta);
 		
 		/*Update stuff here*/
 		camera->update(delta);
