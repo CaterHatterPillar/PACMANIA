35156cac2bd3274f7855290c3b82cb532d551d47
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-10-15 09:53:18 +0200

    Now supports generation of walls

--- SourceCode/DX/GameEntityFactoryDX.cpp
+++ SourceCode/DX/GameEntityFactoryDX.cpp
@@ -120,3 +120,38 @@ GameEntity* GameEntityFactoryDX::createBloodyPill(VecF3 position)
 
 	return entity;
 }
+
+GameEntity* GameEntityFactoryDX::createWall(VecF3 position)
+{
+	GameEntity* entity = new GameEntity();
+	entity->setPosition(position);
+
+	vector<PosNormTex>* vertices	= createVerticesCube();
+	vector<unsigned int>* indices	= createIndicesCube();
+
+	ShaderId vertexShaderId = VERTEX_SHADER_DEFAULT;
+	ShaderId pixelShaderId	= PIXEL_SHADER_DEFAULT;
+	TextureId textureId		= TEXTURE_WALL;
+
+	unsigned int numVertices	= vertices->size();
+	unsigned int numIndices		= indices->size();
+	unsigned int numFaces		= indices->size() / 3;
+	unsigned int stride			= sizeof(PosNormTex);
+	unsigned int offset			= 0;
+
+	GraphicsContainer* graphicsContainer = new GraphicsContainerDX(	vertexShaderId,
+																	pixelShaderId,
+																	textureId,
+																	vertices,
+																	indices,
+																	numVertices,
+																	numIndices,
+																	numFaces,
+																	stride,
+																	offset);
+
+	entity->setGraphicsContainer(graphicsContainer);
+	entity->setMoveBehaviour(NULL);
+
+	return entity;
+}
