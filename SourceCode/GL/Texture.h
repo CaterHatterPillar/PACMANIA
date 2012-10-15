9512337bce2adcbe235992c20cdd541a0280d762
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-10-15 12:06:10 +0200

    Added texid

--- SourceCode/GL/Texture.h
+++ SourceCode/GL/Texture.h
@@ -7,6 +7,8 @@
 
 #include "CommonGL.h"
 
+#include "../TextureId.h"
+
 typedef	struct									
 {
 	GLubyte	* imageData;									// Image Data (Up To 32 Bits)
@@ -15,6 +17,7 @@ typedef	struct
 	GLuint	height;											// Image Height
 	GLuint	texID;											// Texture ID Used To Select A Texture
 	GLuint	type;											// Image Type (GL_RGB, GL_RGBA)
+	TextureId textureId;
 } Texture;	
 
 typedef struct
