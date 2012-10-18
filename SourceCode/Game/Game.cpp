3503c6b5017c258976fbbafb7bfb1dd3a6d88a78
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-10-18 15:17:48 +0200

    Added gametimer to game-class

--- SourceCode/Game/Game.cpp
+++ SourceCode/Game/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 
+#include <iostream>
+
 void Game::run()
 {
 	Maze* maze = entityFac->createMaze();
@@ -8,9 +10,11 @@ void Game::run()
 //	GameEntity* pill = entityFac->createPill(VecF3(4.0f, 0.0f, 0.0f));
 //	GameEntity* bloodyPill = entityFac->createBloodyPill(VecF3(-3.0f, -2.0f, 3.0f)); 
 
+	gameTimer->reset();
+	gameTimer->start();
 	do
 	{
-		double delta = tickTimer();
+		double delta = gameTimer->tick();
 
 		pacman->update(delta);
 //		pill->update(delta);
