53285fd69cd16197f3364af42d84c27e0e059894
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-10-18 16:37:48 +0200

    -

--- SourceCode/Messaging/MsgCamera.h
+++ SourceCode/Messaging/MsgCamera.h
@@ -27,4 +27,35 @@ public:
 	MatF4 Proj() { return proj; }
 };
 
+/*
+class MsgCamera : public Msg
+{
+private:
+	MatF4 view;
+	MatF4 proj;
+
+	VecF3 look;
+protected:
+public:
+	MsgCamera(MatF4 view, MatF4 proj, VecF3 look) : Msg(CAMERA)
+	{
+		this->view = view;
+		this->proj = proj;
+		this->look = look;
+	}
+	MsgCamera(MsgCamera* msgCamera) : Msg(CAMERA)
+	{
+		view = msgCamera->View();
+		proj = msgCamera->Proj();
+		look = msgCamera->Look();
+	}
+	virtual ~MsgCamera() {}
+
+	MatF4 View() { return view; }
+	MatF4 Proj() { return proj; }
+
+	VecF3 Look() { return look; }
+};
+*/
+
 #endif //MSGCAMERA_H
